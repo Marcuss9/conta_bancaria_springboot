f9e2a2083a098cf7d4889b1a2f5becf5b8357ef4
Marcus Cruz <mvcruz0907@gmail.com>
2025-10-29 11:05:05 -0300

    commit swagger

--- src/main/java/com/conta_bancaria_springboot/conta_bancaria_springboot/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/conta_bancaria_springboot/conta_bancaria_springboot/infrastructure/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.conta_bancaria_springboot.conta_bancaria_springboot.infrastructure.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,7 +21,18 @@ public class SwaggerConfig {
                         .version("1.0")
                         .contact(new Contact()
                                 .name("Equipe Conta_Bancaria")
-                                .email("suporte@Conta_Bancaria.com"))
+                                .email("suporte@Conta_Bancaria.com")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                createAPIKeyScheme())
                 );
+    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
